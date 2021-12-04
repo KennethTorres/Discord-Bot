9e85079c2b50e06582adea3dd5cd409997ebec6c
elesteban-os <esteban25agosto@gmail.com>
2021-12-03 23:23:52 -0600

    Init Discord

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-// hola EWHDJSFHJSADHFJDSAKHFKSADJHKJL
+const {Client, Intents, ClientUser} = require('discord.js');
+
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+});
+
+client.once('ready', () => {
+    console.log("BananaBot iniciado");
+});
+
+client.login('OTE0MjEyMDk3ODk2ODk4NjEw.YaJwXQ.meuReOyDpVD-SSNXw36PNoDKrq8');
